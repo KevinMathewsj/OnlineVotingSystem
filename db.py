e58e3d162d8352a90b5ff88a09586bb4b198495b
KevinMathewsj <mathewsk2003@gmail.com>
2026-03-11 04:24:17 +0530

    Update app.py and db.py for Render deployment

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
+import os
 import mysql.connector
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Math@2003",
-    database="voting_db"
+    host=os.environ.get("DB_HOST", "localhost"),      # use env variable or default for local testing
+    user=os.environ.get("DB_USER", "root"),           # same here
+    password=os.environ.get("DB_PASSWORD", ""),       # same here
+    database=os.environ.get("DB_NAME", "voting_db")   # same here
 )
 
 cursor = db.cursor(dictionary=True)
