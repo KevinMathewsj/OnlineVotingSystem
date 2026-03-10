c0c722949bcc63e7e9a89218e12978cff18a455d
KevinMathewsj <mathewsk2003@gmail.com>
2026-03-11 03:56:54 +0530

    Initial commit - Online Voting System

--- app.py
+++ app.py
@@ -290,4 +290,5 @@ def admin_result_pdf():
 
 # ---------------- RUN APP ----------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
